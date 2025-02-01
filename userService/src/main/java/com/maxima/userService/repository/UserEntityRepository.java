403ad54353abda7f6348473e5fc0636ea068dde7
Владимир <vova1210@yandex.ru>
2025-02-01 11:59:30 +0300

    Добавление Сущности, репозитория, зависимостей; редактированиме файла README

--- userService/src/main/java/com/maxima/userService/repository/UserEntityRepository.java
+++ userService/src/main/java/com/maxima/userService/repository/UserEntityRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserEntityRepository extends JpaRepository<UserEntity, Integer> {
+public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
 
 }
