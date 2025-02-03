5f621d19a0cef90dd49e0755ffefc32291f25b7a
Владимир <vova1210@yandex.ru>
2025-02-03 12:24:43 +0300

    Добавление swagger в сервис пользователей

--- userService/src/main/java/com/maxima/userService/controller/UserController.java
+++ userService/src/main/java/com/maxima/userService/controller/UserController.java
@@ -3,6 +3,8 @@ package com.maxima.userService.controller;
 import com.maxima.userService.dto.UserCreateDto;
 import com.maxima.userService.dto.UserViewDto;
 import com.maxima.userService.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Пользователи", description = "Операции CRUD с пользователями ")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
@@ -25,21 +28,25 @@ public class UserController {
 
   private final UserService userService;
 
+  @Operation(summary = "Создание пользователя")
   @PostMapping
   public ResponseEntity<UserViewDto> create(@Valid @RequestBody UserCreateDto userCreateDto) {
     return ResponseEntity.ok(userService.create(userCreateDto));
   }
 
+  @Operation(summary = "Получение списка пользователей")
   @GetMapping
   public ResponseEntity<List<UserViewDto>> getList() {
     return ResponseEntity.ok(userService.getList());
   }
 
+  @Operation(summary = "Получение пользователя по UUID")
   @GetMapping("/{uuid}")
   public ResponseEntity<UserViewDto> getOne(@PathVariable UUID uuid) {
     return ResponseEntity.ok(userService.getOne(uuid));
   }
 
+  @Operation(summary = "Обновление пользователя по UUID")
   @PutMapping("/{uuid}")
   public ResponseEntity<UserViewDto> update(
       @Valid @RequestBody UserCreateDto userCreateDto,
@@ -47,6 +54,7 @@ public class UserController {
     return ResponseEntity.ok(userService.update(userCreateDto, uuid));
   }
 
+  @Operation(summary = "Удаление пользователя по UUID")
   @DeleteMapping("/{uuid}")
   public ResponseEntity<Void> delete(@PathVariable UUID uuid) {
     userService.delete(uuid);
--- userService/src/main/java/com/maxima/userService/dto/UserViewDto.java
+++ userService/src/main/java/com/maxima/userService/dto/UserViewDto.java
@@ -1,5 +1,6 @@
 package com.maxima.userService.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Date;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
@@ -11,13 +12,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Schema(description = "DTO для получения пользователя")
 public class UserViewDto {
 
+  @Schema(description = "UUID пользователя", example = "1")
   private UUID uuid;
 
+  @Schema(description = "Имя пользователя", example = "Иван")
   private String name;
 
+  @Schema(description = "Дата регистрации", example = "2020-01-01")
   private Date registeredAt;
 
+  @Schema(description = "Email пользователя", example = "user@mail.com")
   private String email;
 }
