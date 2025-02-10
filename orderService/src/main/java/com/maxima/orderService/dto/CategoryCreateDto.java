67ba6d07802053fb9eef119a26ff15a3ae3d0b83
Владимир <vova1210@yandex.ru>
2025-02-10 18:29:50 +0300

    Добавление наименований в swagger

--- orderService/src/main/java/com/maxima/orderService/dto/CategoryCreateDto.java
+++ orderService/src/main/java/com/maxima/orderService/dto/CategoryCreateDto.java
@@ -1,14 +1,17 @@
 package com.maxima.orderService.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import java.util.UUID;
 
 /**
 * Дто сущности категории для ввода с фронтэнда
 */
+@Schema(description = "Дто сущности категории для ввода с фронтэнда")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CategoryCreateDto {
+    @Schema(description = "Название категории")
     private String name;
 }
--- orderService/src/main/java/com/maxima/orderService/dto/CategoryDto.java
+++ orderService/src/main/java/com/maxima/orderService/dto/CategoryDto.java
@@ -1,20 +1,25 @@
 package com.maxima.orderService.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import java.util.UUID;
 
 /**
-* Дто сущности категории
-*/
+ * Дто сущности категории
+ */
+@Schema(description = "Дто сущности категории")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CategoryDto {
 
-    private Long id;
+  @Schema(description = "Идентификатор категории", example = "1")
+  private Long id;
 
-    private UUID uuid;
+  @Schema(description = "Глобальный идентификатор категории", example = "fcc49792-9c0b-49f7-9fce-5d9d631d045f")
+  private UUID uuid;
 
-    private String name;
+  @Schema(description = "Название категории", example = "Молочные продукты")
+  private String name;
 
 }
