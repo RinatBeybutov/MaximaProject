258b88ed507149828283ff6b2ef373c043a8450b
temurka <temurka0@yandex.ru>
2025-02-10 21:29:58 +0300

    #6 Изменен способ получения Date

--- userService/src/test/java/com/maxima/userService/testData/UserDtoTestData.java
+++ userService/src/test/java/com/maxima/userService/testData/UserDtoTestData.java
@@ -2,14 +2,15 @@ package com.maxima.userService.testData;
 
 import com.maxima.userService.dto.UserCreateDto;
 import com.maxima.userService.dto.UserViewDto;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.TimeZone;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.UUID;
 
 public class UserDtoTestData {
 
   public static final String USER_NOT_FOUND_MESSAGE = "Пользователь с таким UUID не найден";
+
   public static final int NUMBER_OF_USERS = 3;
 
   public static UserCreateDto userCreateDto() {
@@ -41,18 +42,15 @@ public class UserDtoTestData {
   }
 
   public static UserViewDto vladimirUserDto() {
-    SimpleDateFormat dateFormat =  new SimpleDateFormat("yyyy-MM-dd");
-    dateFormat.setTimeZone(TimeZone.getTimeZone("Europe/London"));
-    try {
-      return UserViewDto.builder()
-          .email("vladimir@gmail.com")
-          .name("Владимир")
-          .uuid(UUID.fromString("39e89964-5f36-46c7-a6c1-324014b75463"))
-          .registeredAt(dateFormat.parse("2021-01-01"))
-          .build();
-    } catch (ParseException e) {
-      throw new RuntimeException(e);
-    }
+    return UserViewDto.builder()
+        .email("vladimir@gmail.com")
+        .name("Владимир")
+        .uuid(UUID.fromString("39e89964-5f36-46c7-a6c1-324014b75463"))
+        .registeredAt(Date.from(LocalDate.parse("2021-01-01")
+                                    .atStartOfDay()
+                                    .atZone(ZoneId.of("Europe/London"))
+                                    .toInstant()))
+        .build();
   }
 
 }
