d6d250175d3a781f6cbe2af3238e0262fe32aae7
fguper <kkk@example.com>
2025-02-12 12:23:56 +0300

    1. добавлено COMMENT ON в миграции 2. изменены UUID и названия категорий в миграции

--- orderService/src/main/resources/db/migration/V1_1__create_schema_and_tables.sql
+++ orderService/src/main/resources/db/migration/V1_1__create_schema_and_tables.sql
@@ -1,9 +1,19 @@
 CREATE SCHEMA IF NOT EXISTS order_service AUTHORIZATION postgres;
 
-create table if not exists order_service.categories( -- таблица категорий
-     id int primary key,    -- идентификатор записи в БД, первичный ключ
-     uuid UUID,             -- UUID - идентификатор записи
-     name varchar           -- название категории
+create table if not exists order_service.categories(
+     id int primary key,
+     uuid UUID,
+     name varchar
 );
 
-create sequence order_service.cat_id_seq; -- sequence для генерации значения поля id
+create sequence order_service.cat_id_seq;
+
+COMMENT ON TABLE order_service.categories IS 'таблица категорий';
+
+COMMENT ON COLUMN order_service.categories.id IS 'идентификатор записи в БД, первичный ключ';
+
+COMMENT ON COLUMN order_service.categories.UUID IS 'UUID - идентификатор записи';
+
+COMMENT ON COLUMN order_service.categories.name IS 'название категории';
+
+COMMENT ON sequence order_service.cat_id_seq IS 'sequence для генерации значения поля id';
