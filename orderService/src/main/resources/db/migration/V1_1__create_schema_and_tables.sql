5f67937012a9d51dd6ea2d10e5b47ce08056e109
fguper <kkk@example.com>
2025-02-12 11:09:19 +0300

    исправление замечаний

--- orderService/src/main/resources/db/migration/V1_1__create_schema_and_tables.sql
+++ orderService/src/main/resources/db/migration/V1_1__create_schema_and_tables.sql
@@ -1,9 +1,9 @@
 CREATE SCHEMA IF NOT EXISTS order_service AUTHORIZATION postgres;
 
 create table if not exists order_service.categories(
-     id int primary key,
-     uuid UUID,
-     name varchar
+     id int primary key,    -- идентификатор записи в БД, первичный ключ
+     uuid UUID,             -- UUID - идентификатор записи
+     name varchar           -- название категории
 );
 
-create sequence order_service.cat_id_seq;
+create sequence order_service.cat_id_seq; -- sequence для генерации значения поля id
