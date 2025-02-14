8d67f356180eb8ba4d73c35d969dab10fe9d3523
TatyanaMal <malfanova46@gmail.com>
2025-02-14 16:26:46 +0300

    Добавлена сущность CategoryEntity

--- orderService/src/main/java/com/maxima/orderService/entity/CategoryEntity.java
+++ orderService/src/main/java/com/maxima/orderService/entity/CategoryEntity.java
@@ -3,11 +3,23 @@ package com.maxima.orderService.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 @Entity
 @Table(name = "categories", schema = "order_service")
-public class CategoryEntity extends BaseEntity{
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "uuid", unique = true)
+    private UUID uuid;
 
     private String name;
 
+    @PrePersist
+    private void generateUuid() {
+        setUuid(UUID.randomUUID());
+    }
 }
