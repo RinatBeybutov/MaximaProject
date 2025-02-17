f5a0a040976bcf3322788933671c74082070f2dc
TatyanaMal <malfanova46@gmail.com>
2025-02-17 22:07:58 +0300

    Исправлены конфликты и добавлен родительский класс

--- orderService/src/main/java/com/maxima/orderService/entity/CategoryEntity.java
+++ orderService/src/main/java/com/maxima/orderService/entity/CategoryEntity.java
@@ -3,23 +3,11 @@ package com.maxima.orderService.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.util.UUID;
-
 @Data
 @Entity
 @Table(name = "categories", schema = "order_service")
-public class CategoryEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "uuid", unique = true)
-    private UUID uuid;
+public class CategoryEntity extends BaseEntity{
 
     private String name;
 
-    @PrePersist
-    private void generateUuid() {
-        setUuid(UUID.randomUUID());
-    }
 }
