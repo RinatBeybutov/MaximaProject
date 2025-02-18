f4646ac5cbd0607aa7e8427b49eafe3e724b6012
Acer <krc@nov.ru>
2025-02-18 21:06:02 +0300

    добавлен Lombok в класс TestData

--- orderService/src/test/java/com/maxima/orderService/TestData.java
+++ orderService/src/test/java/com/maxima/orderService/TestData.java
@@ -4,38 +4,24 @@ import com.maxima.orderService.dto.CategoryCreateDto;
 import com.maxima.orderService.dto.CategoryDto;
 
 import java.util.UUID;
+import lombok.Getter;
 
 public class TestData {
     public static final String testUUID="fcc49792-9c0b-49f7-9fce-5d9d631d042f";
     public static final String testUUID2="fcc49792-9c0b-49f7-9fce-5d9d631d044f";
     public static final String CATEGORY_NAME="Тестовая категория1";
+    @Getter
     private static final CategoryCreateDto categoryCreateDto = new CategoryCreateDto("Тестовая категория");
 
-    public static CategoryCreateDto getCategoryCreateDto(){
-        return categoryCreateDto;
-    }
-
+    @Getter
     private static final CategoryDto categoryDto = new CategoryDto(4L, UUID.fromString("fcc49792-9c0b-49f7-9fce-5d9d631d042f"),"Чипсы");
 
-    public static CategoryDto getCategoryDto(){
-        return categoryDto;
-    }
-
+    @Getter
     private static final CategoryDto categoryDtoForCreate = new CategoryDto(10L, UUID.fromString("1cc49792-9c0b-49f7-9fce-5d9d631d042f"),"Тестовая категория");
 
-    public static CategoryDto getCategoryDtoForCreate(){
-        return categoryDtoForCreate;
-    }
-
+    @Getter
     private static final CategoryDto categoryDtoForUpdate = new CategoryDto(10L, UUID.fromString("1cc49792-9c0b-49f7-9fce-5d9d631d042f"),CATEGORY_NAME);
 
-    public static CategoryDto getCategoryDtoForUpdate(){
-        return categoryDtoForUpdate;
-    }
-
+    @Getter
     private static final CategoryDto categoryDtoFirstInList = new CategoryDto(1L, UUID.fromString("fcc49792-9c0b-49f7-9fce-5d9d631d045f"),"Напитки");
-
-    public static CategoryDto getCategoryDtoFirstInList(){
-        return categoryDtoFirstInList;
-    }
 }
