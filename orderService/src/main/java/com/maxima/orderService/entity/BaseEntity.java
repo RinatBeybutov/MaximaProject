7e52ee2e9d34513736b16d9cee1c2b3a60128cf2
fguper <kkk@example.com>
2025-02-19 13:59:36 +0300

    удалены комментарии из кода сущностей

--- orderService/src/main/java/com/maxima/orderService/entity/BaseEntity.java
+++ orderService/src/main/java/com/maxima/orderService/entity/BaseEntity.java
@@ -1,10 +1,5 @@
 package com.maxima.orderService.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.*;
 
 import java.util.UUID;
@@ -13,13 +8,6 @@ import lombok.Data;
 @Data
 @MappedSuperclass
 public class BaseEntity {
-  /*@Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-
-  @Column(name = "uuid", unique = true)
-  private UUID uuid;*/
-
     @Id
     @Column(name="id",nullable=false)   
     @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="sgu")
--- orderService/src/main/java/com/maxima/orderService/entity/CategoryEntity.java
+++ orderService/src/main/java/com/maxima/orderService/entity/CategoryEntity.java
@@ -10,8 +10,4 @@ import lombok.*;
 @Table(name = "categories", schema = "order_service")
 public class CategoryEntity extends BaseEntity{
     private String name;
-
-    /*public CategoryEntity(String name){
-        this.name=name;
-    }*/
 }
