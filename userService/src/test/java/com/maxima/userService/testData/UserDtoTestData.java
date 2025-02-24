f6d0a793258a2fa311a0229b4a13e89997f68462
Владимир <vova1210@yandex.ru>
2025-02-24 16:40:17 +0300

    #31 Добавление Локализации ошибок в сервисе пользователей, добавление локализации добавление MessageConfig добавление ключей для выбора языка, изменение глобального перехватчика ошибок добавление ключа в UserRepository

--- userService/src/test/java/com/maxima/userService/testData/UserDtoTestData.java
+++ userService/src/test/java/com/maxima/userService/testData/UserDtoTestData.java
@@ -11,7 +11,7 @@ import java.util.UUID;
  */
 public class UserDtoTestData {
 
-  public static final String USER_NOT_FOUND_MESSAGE = "Пользователь с таким UUID не найден";
+  public static final String USER_NOT_FOUND_MESSAGE = "User not found";
 
   public static final int NUMBER_OF_USERS = 3;
 
