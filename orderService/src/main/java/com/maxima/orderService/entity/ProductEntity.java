2b11d335d8ebe4cff289f5e03670ccd31e5ba8b9
Владимир <vova1210@yandex.ru>
2025-03-02 21:19:06 +0300

    Выгружаю commit для того что бы завтра продолжить на работе

--- orderService/src/main/java/com/maxima/orderService/entity/ProductEntity.java
+++ orderService/src/main/java/com/maxima/orderService/entity/ProductEntity.java
@@ -2,8 +2,10 @@ package com.maxima.orderService.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +22,9 @@ public class ProductEntity extends BaseEntity {
 
   private String name;
 
-  @Column(name = "category_id")
-  private UUID categoryId;
+  @Column(name = "category_id", updatable = false, insertable = false)
+  private Long categoryid;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private CategoryEntity category;
 }
