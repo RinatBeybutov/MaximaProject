2565a979bf0780c7340490d401698e897b968e63
Владимир <vova1210@yandex.ru>
2025-03-04 21:54:22 +0300

    #25 фикс стиля кода и наименования миграций

--- orderService/src/main/resources/db/migration/V1.5_create_table_order.sql
+++ orderService/src/main/resources/db/migration/V1.5_create_table_order.sql
@@ -1,34 +1,19 @@
-create table if not exists order_service.orders
-(
-    id
-    BIGSERIAL
-    PRIMARY
-    KEY,
-    uuid
-    UUID,
-    date
-    DATE,
-    status
-    INTEGER
-    user_uuid
-    UUID
+create table if not exists order_service.orders(
+    id BIGSERIAL PRIMARY KEY,
+    uuid UUID,
+    date DATE,
+    status INTEGER
+    user_uuid UUID
 );
 
-COMMENT
-ON TABLE order_service.orders IS 'таблица заказов';
+COMMENT ON TABLE order_service.orders IS 'таблица заказов';
 
-COMMENT
-ON COLUMN order_service.orders.id IS 'идентификатор записи в БД, первичный ключ';
+COMMENT ON COLUMN order_service.orders.id IS 'идентификатор записи в БД, первичный ключ';
 
-COMMENT
-ON COLUMN order_service.orders.UUID IS 'UUID - идентификатор записи';
+COMMENT ON COLUMN order_service.orders.UUID IS 'UUID - идентификатор записи';
 
-COMMENT
-ON COLUMN order_service.orders.date IS 'дата заказа';
+COMMENT ON COLUMN order_service.orders.date IS 'дата заказа';
 
-COMMENT
-ON COLUMN order_service.orders.status IS 'статус заказа';
+COMMENT ON COLUMN order_service.orders.status IS 'статус заказа';
 
-COMMENT
-ON COLUMN order_service.orders.user_uuid IS 'UUID пользователя';
-
+COMMENT ON COLUMN order_service.orders.user_uuid IS 'UUID пользователя';
