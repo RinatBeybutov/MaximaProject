eee8d5cb47a9e6d4361ae9753d5436fa9b7835ec
TatyanaMal <malfanova46@gmail.com>
2025-03-08 23:38:40 +0300

    #20 Добавлены уточнения для колонн

--- orderService/src/main/resources/db/migration/V1.3__create_table_products.sql
+++ orderService/src/main/resources/db/migration/V1.3__create_table_products.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS order_service.products (
 );
 
 COMMENT ON TABLE order_service.products IS 'Таблица продуктов';
-COMMENT ON COLUMN order_service.products IS 'Идентификатор продукта';
-COMMENT ON COLUMN order_service.products IS 'Глобальный идентификатор продукта';
-COMMENT ON COLUMN order_service.products IS 'Название продукта';
-COMMENT ON COLUMN order_service.products IS 'Глобальный идентификатор категории, к которой относится продукт';
+COMMENT ON COLUMN order_service.products.id IS 'Идентификатор продукта';
+COMMENT ON COLUMN order_service.products.uuid IS 'Глобальный идентификатор продукта';
+COMMENT ON COLUMN order_service.products.name IS 'Название продукта';
+COMMENT ON COLUMN order_service.products.category_id IS 'Глобальный идентификатор категории, к которой относится продукт';
