18e74a206f1f3cde8ccde8663b0e3e652f771a97
Ринат <ignat934@gmail.com>
2025-03-14 10:23:23 +0300

    #24: фикс миграций

--- orderService/src/main/resources/db/migration/V1.5__create_table_products.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists order_service.products(
-     id BIGSERIAL PRIMARY KEY,
-     uuid UUID UNIQUE,
-     name varchar,
-     category_id INT
-);
-
-alter table order_service.products add constraint fk_products_category_id foreign key(category_id) references order_service.categories(id);
