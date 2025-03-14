276c83b23fa8d23057f6f8fc03f09dece3c22faf
Владимир <vova1210@yandex.ru>
2025-03-14 19:14:38 +0300

    #30 фикс аннотации MetricsApiDelegateImpl.java

--- metricsService/src/main/java/com/maxima/metricsservice/api/MetricsApiDelegateImpl.java
+++ metricsService/src/main/java/com/maxima/metricsservice/api/MetricsApiDelegateImpl.java
@@ -3,14 +3,14 @@ package com.maxima.metricsservice.api;
 import com.maxima.metricsservice.service.MetricsService;
 import com.petProject.MetricsService.controller.MetricsApiDelegate;
 import com.petProject.MetricsService.dto.OrderMetricsDto;
-import io.swagger.v3.oas.annotations.servers.Servers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 
 /**
  * Реализация Апи для метрик заказов.
  */
-@Servers
+@Component
 @RequiredArgsConstructor
 public class MetricsApiDelegateImpl implements MetricsApiDelegate {
 
