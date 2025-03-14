6442f72b9825060e6ce04bc5a151f071b6561155
root <root@78.37.24.61>
2025-03-14 11:27:44 +0300

    #1 добавлено добавлено on delete cascade для заказов в таблицу ProductToOrder

--- orderService/src/main/resources/db/migration/V1.6__create_table_productToOrder.sql
+++ orderService/src/main/resources/db/migration/V1.6__create_table_productToOrder.sql
@@ -6,4 +6,4 @@ create table if not exists order_service.product_to_order(
 );
 
 alter table order_service.product_to_order add foreign key (product_id) references order_service.products(id);
-alter table order_service.product_to_order add foreign key (order_id) references order_service.orders(id);
+alter table order_service.product_to_order add foreign key (order_id) references order_service.orders(id) on delete cascade;
